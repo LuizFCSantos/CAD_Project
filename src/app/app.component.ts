7e7df1b505d03d3ab46844e7ba2991cd82a1c846
LuizFCSantos <luiz.scampos17@gmail.com>
2024-08-13 15:50:33 -0300

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+// import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
