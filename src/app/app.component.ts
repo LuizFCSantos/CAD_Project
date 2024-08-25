2903d2601e2e0d245e120cbdee56bf2469f41857
LuizFCSantos <luiz.scampos17@gmail.com>
2024-08-25 11:29:30 -0300

    cadastroPronto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+// import { LoginPageComponent } from './login-page/login-page.component';
+import { CadastroPageComponent } from "./cadastro-page/cadastro-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent],
+  imports: [RouterOutlet, CadastroPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/calculadora-page/calculadora-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculadora-page.component.html',
+  styleUrl: './calculadora-page.component.scss'
+})
+export class CalculadoraPageComponent {
+
+}
